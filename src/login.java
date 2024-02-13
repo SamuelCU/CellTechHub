f12606dfd09d2d5a630a2ebd5486428c180c6d77
Estefanía Sánchez <estefi2000ms2@gmail.com>
2024-02-13 10:31:05 -0500

    creacion de pantallas v1

---
+++ src/login.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class login {
+    private JPanel inicio;
+    private JComboBox comboBox1;
+    private JTextField userF;
+    private JPasswordField passwordF;
+    private JButton ENTERButton;
+    private JLabel user;
+    private JLabel password;
+}
---
+++ src/venta.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class venta {
+    private JPanel venta;
+    private JTextField codigoF;
+    private JTextField categoriaF;
+    private JTable contenido;
+    private JButton nextButton;
+    private JTextField pagoF;
+    private JLabel img;
+    private JLabel codigo;
+    private JLabel pago;
+    private JLabel categoria;
+}
