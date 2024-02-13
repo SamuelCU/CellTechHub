ba0764c1fcd08de005a53c951e2da7eb04e00096
Estefanía Sánchez <estefi2000ms2@gmail.com>
2024-02-13 12:29:32 -0500

    creacion de pantallas v2

---
+++ src/cliente.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class cliente {
+    private JPanel panel1;
+    private JTextField clienteF;
+    private JTable clienteT;
+    private JButton nextButton;
+    private JButton registrarButton;
+}
---
+++ src/registro.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class registro {
+    private JPanel registro;
+    private JTextField cedulaF;
+    private JTextField nombreF;
+    private JTextField telefonoF;
+    private JTextField direccionF;
+    private JButton registarButton;
+}
