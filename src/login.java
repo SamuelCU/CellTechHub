5132ce1916ed5f7f2cbb36c39704f645c252491a
KevinDaniel10 <kevin.cola@epn.edu.ec>
2024-02-17 15:36:13 -0500

    funcionalidad_combobox

--- src/login.java
+++ src/login.java
@@ -1,11 +1,31 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class login {
-    private JPanel inicio;
-    private JComboBox comboBox1;
-    private JTextField userF;
-    private JPasswordField passwordF;
-    private JButton ENTERButton;
-    private JLabel user;
-    private JLabel password;
+    public JPanel loginpanel;
+    private JComboBox comboBoxlogin;
+    private JTextField usertext;
+    private JPasswordField passwordtext;
+    private JButton ingressarboton;
+    private JLabel Userl;
+    private JLabel contrasenal;
+    private JLabel modol;
+
+    public login() {
+        comboBoxlogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Obtener la opción seleccionada
+                String selectedOption = (String) comboBoxlogin.getSelectedItem();
+
+                // Abrir la ventana correspondiente
+                if (selectedOption.equals("Cajero")) {
+                    Userl.setText("Cajero: ");
+                } else if (selectedOption.equals("Administrativo")) {
+                    Userl.setText("Admistrador: ");
+                }
+            }
+        });
+    }
 }
