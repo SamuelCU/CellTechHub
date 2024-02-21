7333ce8ecd40fe456af3f16dff6dba901c42922b
KevinDaniel10 <kevin.cola@epn.edu.ec>
2024-02-21 13:56:08 -0500

    Cajero busqueda id
    Se agrego la busqueda de un cajero en el form administrativo

--- src/administrador.java
+++ src/administrador.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.Objects;
 
 public class administrador {
     protected JPanel administrador;
@@ -17,7 +20,24 @@ public class administrador {
         nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                // Obtener la cédula ingresada
+                String cedula = cedula1F.getText();
+                String query = "SELECT id_empleado FROM empleados WHERE id_empleado = ?";
+                try(Connection connection = DriverManager.getConnection("jdbc:mysql://utrknxklzvib6amk:KkyQew23qotJ6GAkmksf@bqjaf6disbf0cayzptja-mysql.services.clever-cloud.com:3306/bqjaf6disbf0cayzptja", "utrknxklzvib6amk", "KkyQew23qotJ6GAkmksf")){
+                    try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+                        preparedStatement.setString(1,cedula);
+                        try(ResultSet resultSet = preparedStatement.executeQuery()){
+                            if(resultSet.next()){
+                                System.out.println("Usuario encontrado");
+                            }else{
+                                System.out.println("Usuario no encontrado");
+                                cedula1F.setText("");
+                            }
+                        }
+                    }
+                }catch(SQLException ex){
+                    ex.printStackTrace();
+                }
             }
         });
     }
