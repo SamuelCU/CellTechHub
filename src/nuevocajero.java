ef2a80f78bdcb9fbeefd87a6e9094210ca025a2e
KevinDaniel10 <kevin.cola@epn.edu.ec>
2024-02-24 11:29:12 -0500

    Inserccion de Nuevo cajero a la base de datos
    Funcionalidad agregada para ingresar nuevo cajero

--- src/nuevocajero.java
+++ src/nuevocajero.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class nuevocajero {
     private JButton ingresarButton;
@@ -16,6 +17,8 @@ public class nuevocajero {
     private JTextField contraseniatext;
     private JTextField ccontrasenoatext;
     public JPanel nuevocajeropanel;
+    private JTextField cedulatext;
+    private JTextField ccedulatext;
 
     public nuevocajero() {
         regresarButton.addActionListener(new ActionListener() {
@@ -35,28 +38,39 @@ public class nuevocajero {
         ingresarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                /*int idn;
+                int idn= Integer.parseInt(cedulatext.getText());
                 String nombren= nombretext.getText();
                 String contrasenian=contraseniatext.getText();
-                try(Connection connection= DriverManager.getConnection("jdbc:mysql://utrknxklzvib6amk:KkyQew23qotJ6GAkmksf@bqjaf6disbf0cayzptja-mysql.services.clever-cloud.com:3306/bqjaf6disbf0cayzptja", "utrknxklzvib6amk", "KkyQew23qotJ6GAkmksf")){
-                    String sql="INSERT INTO empleados(id_empleado,nombre,contrasenia,tipo)values(?,?,?,?)";
-                    try(PreparedStatement preparedStatement=connection.prepareStatement(sql)){
-                        preparedStatement.setInt(1,idn);
-                        preparedStatement.setString(2,nombren);
-                        preparedStatement.setString(3,contrasenian);
-                        preparedStatement.setString(4,"Cajero");
-                        int filasAfectadas=preparedStatement.executeUpdate();
-                        if(filasAfectadas>0){
-                            System.out.println("Insercion exitosa");
-                        }else {
-                            System.out.println("No se pudo insertar");
+                if(Objects.equals(cedulatext.getText(),ccedulatext.getText())&
+                    Objects.equals(nombretext.getText(),cnombretext.getText())&
+                        Objects.equals(contraseniatext.getText(),ccontrasenoatext.getText())
+                ){
+                    try(Connection connection= DriverManager.getConnection("jdbc:mysql://utrknxklzvib6amk:KkyQew23qotJ6GAkmksf@bqjaf6disbf0cayzptja-mysql.services.clever-cloud.com:3306/bqjaf6disbf0cayzptja", "utrknxklzvib6amk", "KkyQew23qotJ6GAkmksf")){
+                        String sql="INSERT INTO empleados(id_empleado,nombre,contrasenia,tipo)values(?,?,?,?)";
+                        try(PreparedStatement preparedStatement=connection.prepareStatement(sql)){
+                            preparedStatement.setInt(1,idn);
+                            preparedStatement.setString(2,nombren);
+                            preparedStatement.setString(3,contrasenian);
+                            preparedStatement.setString(4,"Cajero");
+                            int filasAfectadas=preparedStatement.executeUpdate();
+                            if(filasAfectadas>0){
+                                System.out.println("Insercion exitosa");
+                            }else {
+                                System.out.println("No se pudo insertar");
+                            }
+                        }catch (SQLException ex){
+                            ex.printStackTrace();
                         }
                     }catch (SQLException ex){
                         ex.printStackTrace();
                     }
-                }catch (SQLException ex){
-                    ex.printStackTrace();
-                }*/
+                }else {
+                    JOptionPane optionPane = new JOptionPane("No coincioden las credenciales", JOptionPane.ERROR_MESSAGE);
+                    // Mostrar el cuadro de diálogo
+                    JDialog dialog = optionPane.createDialog("Error");
+                    dialog.setResizable(false);
+                    dialog.setVisible(true);
+                }
             }
         });
         salirButton.addActionListener(new ActionListener() {
