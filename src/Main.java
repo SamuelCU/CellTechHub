87b3dd6e74d286d5731fb11dd35af6279bf9993b
Samuel Cuti <samcuti.03@gmail.com>
2024-03-04 14:49:28 -0500

    Verificacion

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Login");
-        frame.setContentPane(new cajeropantalla().acajeropanel);
+        frame.setContentPane(new login().loginpanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(850,550);
         frame.setLocationRelativeTo(null);
